f58049a184d851eb448e0e07b5db3a948bfac6f9
Selahattin Unal <unalselahattin.pro@gmail.com>
2026-04-02 09:30:12 +0200

    MAJ 02/04

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,4 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    // On force l'écoute sur la bonne adresse IP Wi-Fi du Mac
-    // Comme ça npm run dev fonctionne directement sans options
-    host: '0.0.0.0'
-  }
 })
